cba67196c0b6932a94ec9c96e67e4341ca84e041
Lucy Gillespie <lucy@unlucy.com>
2024-08-08 21:48:52 -0700

    blah

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
+#!/usr/bin/env bash
+
 age -R - -o key.txt.age key.txt < recipients 
 age -R - -o secrets.html.age secrets.html < recipients 
--- rebuild.sh
+++ rebuild.sh
@@ -1,2 +1,4 @@
+#!/usr/bin/env bash
+
 age -i ~/keys/age-yubikey.txt -d secrets.html.age
 age -i ~/keys/age-yubikey.txt -d key.txt.age
